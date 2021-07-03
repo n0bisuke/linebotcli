ad4cb6cef0c63ff121380661369176a1633c963d
n0bisuke <rsksound@gmail.com>
2021-07-03 21:20:27 +0900

    init

--- bin/index.js
+++ bin/index.js
@@ -1,30 +1,49 @@
 #!/usr/bin/env node
 'use strict';
 
-const axios = require('axios');
-const LINE_WEBHOOK_SET_API = `https://api.line.me/v2/bot/channel/webhook/endpoint`;
-const LINE_CHANNEL_ACCESS_TOKEN = process.env.TOKEN; //コマンドから
+const program = require('commander')
+const p = require('../package.json')
+const webhook = require('./actions/webhook')
 
-let config = {
-    url: LINE_WEBHOOK_SET_API,
-    method: 'put',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
-    },
-    data: {
-        'endpoint': process.env.WEBHOOK_URL
-    }
-}
+// バージョン情報
+program
+  .version(p.version, '-v, --version')
 
-const main = async () => {
-    try {
-        const res = await axios.request(config);
-        console.log(res.data);
-    } catch (error) {
-        console.log(error);
-    }
+// webhookコマンド
+program.command(`webhook`)
+    .description('Change the LINE BOT webhook URL')
+    .option('-t, --token <Access Token>', 'Access Token')
+    .option('-u, --url <Webhook URL>', 'Webhook URL')
+    // .option('-d, --debug', 'display some debugging')
+    .action((name, options, command) => {
+        const params = {
+            token: name.token,
+            url: name.url
+        }
+        return webhook(params);
+    })
+    .action(res => console.log(`\n Webhook URLを「${res.url}」に変更しました。`))
+    // .option('-p, --pizza-type <type>', 'flavour of pizza')
+    // .argument('<username>', 'user to login')
+    // .action(user => console.log(`hoge`,user));
+    // .action(() => webhook())
+    // .action((name, options, command) => {
+    //     const options2 = program.opts();
+    //     console.log(options2.pizzaType);
+        // if (options.debug) {
+        //     console.error('Called %s with options %o', command.name(), options);
+        //   }
+        //   const title = options.title ? `${options.title} ` : '';
+        //   console.log(`Thank-you ${title}${name}`);
+    // })
 
-}
+// const options = program.opts();
+// console.log(options);
+// if (options.pizzaType) console.log(`- ${options.pizzaType}`);
 
-main();
+// // fugaコマンド
+// program.command('fuga')
+//   .description('2つ目のコマンドです')
+//   .action(() => console.log('fugaが実行されました'))
+
+program.parse(process.argv)
